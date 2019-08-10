bf078cfce6afba21fe6f67a14a03393cbd88a8ce
Jorge H. F. Pereira <j.pereira@klickmeister.de>
2019-08-11 00:59:12 +0200

    Verlagere Style-Definitionen von html und body-Element in den Component-Entry-Point

--- src/html.jsx
+++ src/html.jsx
@@ -1,8 +1,28 @@
+/* Development-Mode: For changes to take effect, development-mode has to be restarted */
+
 import React from 'react';
+import { css } from '@emotion/core';
+import { mediaQuery } from '~/styles/mixins/media-query';
+
+const defaultStyle = css`
+  background-color: #333333;
+  padding: 1.25rem;
+  font-family: "IBMPlexSansLight";
+  color: #B4B4B4;
+  font-size: 80%;
+
+  ${mediaQuery.sm()} {
+    padding: 1.6rem;
+    font-size: 100%;
+  }
+`;
 
 export default function HTML(props) {
   return (
-    <html {...props.htmlAttributes}>
+    <html
+      {...props.htmlAttributes}
+      css={ defaultStyle }
+    >
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
@@ -15,7 +35,10 @@ export default function HTML(props) {
         <link rel="stylesheet" type="text/css" href="/styles/global.css" />
         {props.headComponents}
       </head>
-      <body {...props.bodyAttributes}>
+      <body
+        {...props.bodyAttributes}
+        css={ defaultStyle }
+      >
         {props.preBodyComponents}
         <noscript key="noscript" id="gatsby-noscript">
           This app works best with JavaScript enabled.
