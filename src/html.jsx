24ad1fa52d31eea81a2069d2eacf88ac9c724136
Jorge H. F. Pereira <j.pereira@klickmeister.de>
2019-09-03 00:09:30 +0200

    Binde Basis-Style ein

--- src/html.jsx
+++ src/html.jsx
@@ -1,27 +1,11 @@
 /* Development-Mode: For changes to take effect, development-mode has to be restarted */
 
 import React from 'react';
-import { css } from '@emotion/core';
-import { mediaQuery } from '~/styles/mixins/media-query';
-
-const defaultStyle = css`
-  background-color: #333333;
-  padding: 1.25rem;
-  font-family: "IBMPlexSansLight";
-  color: #B4B4B4;
-  font-size: 80%;
-
-  ${mediaQuery.sm()} {
-    padding: 1.6rem;
-    font-size: 100%;
-  }
-`;
 
 export default function HTML(props) {
   return (
     <html
       {...props.htmlAttributes}
-      css={ defaultStyle }
     >
       <head>
         <meta charSet="utf-8" />
@@ -37,7 +21,6 @@ export default function HTML(props) {
       </head>
       <body
         {...props.bodyAttributes}
-        css={ defaultStyle }
       >
         {props.preBodyComponents}
         <noscript key="noscript" id="gatsby-noscript">
