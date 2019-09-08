508afda69286c41d5e84a120774b9c8387175465
Jorge H. F. Pereira <j.pereira@klickmeister.de>
2019-09-08 22:31:48 +0200

    Erweitere Navigation-Komponente um Items

--- src/components/molecules/navigation/navigation.jsx
+++ src/components/molecules/navigation/navigation.jsx
@@ -1,23 +1,66 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Logo from '~/components/atoms/logo';
+import Link from '~/components/atoms/link';
 
-export default () => (
-  <nav
-    className="navigation navbar has-shadow is-spaced"
-    role="navigation"
-    aria-label="main navigation"
-  >
-    <div className="navbar-brand">
-      <Link
-        className="navbar-item"
-        to="/"
-      >
-        <Logo />
-      </Link>
-    </div>
 
-    <div className="navbar-menu"></div>
-  </nav>
-);
+export default () => {
+  /* TODO: Pass through as parameter (?) */
+  const navStructure = [
+    {
+      title: 'Gemälde',
+      to: 'http://lucascranach.org/gallery',
+    },
+    {
+      title: 'Archivalien',
+      to: 'http://lucascranach.org/archival-documents',
+    },
+    {
+      title: 'Literatur',
+      to: 'http://lucascranach.org/publications',
+    },
+    {
+      title: 'Grafiken',
+      to: '/',
+    },
+  ];
+
+  return (
+    <nav
+      className="navigation navbar has-shadow is-spaced"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="navbar-brand">
+        <Link
+          className="navbar-item"
+          to="/"
+        >
+          <Logo />
+        </Link>
+      </div>
+
+      <div className="navbar-menu">
+        <div className="navbar-start">
+          {
+            navStructure.map(
+              (item) => (
+                <Link
+                  className="navbar-item"
+                  to={ item.to }
+                  key={ item.to }
+                  activeClassName="is-active"
+                >
+                  { item.title }
+                </Link>
+              ),
+            )
+          }
+        </div>
+
+        <div className="navbar-end">
+        </div>
+      </div>
+    </nav>
+  );
+};
