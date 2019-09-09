5d71e7643ec9c8d8e1112b52f3aec4fa6a866522
Jorge H. F. Pereira <j.pereira@klickmeister.de>
2019-09-09 02:30:47 +0200

    Gib Grundinformationen in Page-Template aus

---
+++ src/components/atoms/definition-list/definition-list.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './definition-list.scss';
+
+const DefinitionList = ({ children }) => (
+  <dl
+    className="definition-list"
+  >
+    { children }
+  </dl>
+);
+
+DefinitionList.Entry = ({ term, definition }) => (
+  <React.Fragment>
+    <dt className="term">{ term }</dt>
+    <dd className="definition">{ definition }</dd>
+  </React.Fragment>
+);
+
+export default DefinitionList;
--- src/templates/page.jsx
+++ src/templates/page.jsx
@@ -3,14 +3,27 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 
-import ZoomImage from '~/components/atoms/zoom-image';
 import Navigation from '~/components/molecules/navigation';
+import ZoomImage from '~/components/atoms/zoom-image';
+import DefinitionList from '~/components/atoms/definition-list';
 
 
 const PageTemplate = ({ pageContext }) => {
   const graphic = pageContext;
 
+  const inventor = graphic.involvedPersons.find(person => person.role === 'Inventor');
+
   const title = (graphic.titles[0] && graphic.titles[0].title) || '';
+  const subtitle = inventor ? inventor.name : ' ';
+  const description = graphic.description || ' ';
+
+  const {
+    classification,
+    dating,
+    dimensions,
+    inventoryNumber,
+    objectName,
+  } = graphic;
 
   return (
     <div
@@ -33,7 +46,41 @@ const PageTemplate = ({ pageContext }) => {
           </div>
 
           <div className="column">
-            <p>{ title }</p>
+            <h1 className="title">{ title }</h1>
+            <h2 className="subtitle">{ subtitle }</h2>
+
+            <div className="content">
+              { description }
+            </div>
+
+            <div className="content">
+              <DefinitionList>
+                <DefinitionList.Entry
+                  term="Klassifikation"
+                  definition={ classification.classification }
+                />
+
+                <DefinitionList.Entry
+                  term="Datiert"
+                  definition={ dating.dated }
+                />
+
+                <DefinitionList.Entry
+                  term="Maße"
+                  definition={ dimensions }
+                />
+
+                <DefinitionList.Entry
+                  term="Inventarnummer"
+                  definition={ inventoryNumber }
+                />
+
+                <DefinitionList.Entry
+                  term="Objektname"
+                  definition={ objectName }
+                />
+              </DefinitionList>
+            </div>
           </div>
         </div>
       </div>
