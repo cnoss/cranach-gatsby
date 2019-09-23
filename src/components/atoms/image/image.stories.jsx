7ebc0ad371ff13b3183ce169ef0f0f91d96f837a
Jorge H. F. Pereira <j.pereira@klickmeister.de>
2019-09-23 21:43:48 +0200

    Image: Erweiterung der stories

--- src/components/atoms/image/image.stories.jsx
+++ src/components/atoms/image/image.stories.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
+import {
+  withKnobs,
+  files,
+  text,
+} from '@storybook/addon-knobs';
+
 import '~/styles/main.scss';
 
 import Image from '.';
@@ -9,25 +15,64 @@ const dummyImage = 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQEBLAEsAAD/2wBDAAIBA
 
 
 storiesOf('Atoms/Image', module)
-  .add('with src', () => (
-    <div style={{ width: '200px' }}>
-      <Image
-        src={ dummyImage }
-      ></Image>
-    </div>
-  ))
-  .add('with alt text', () => (
-    <div style={{ width: '200px' }}>
-      <Image
-        alt="Test"
-      ></Image>
-    </div>
-  ))
-  .add('with caption', () => (
-    <div style={{ width: '200px' }}>
-      <Image
-        src={ dummyImage }
-        caption="A caption text"
-      ></Image>
-    </div>
-  ));
+  .addDecorator(withKnobs)
+  .add('with src', () => {
+    const images = files('Image', 'image/*', [ dummyImage ]);
+
+    return (
+      <div style={{ width: '300px' }}>
+        <Image
+          src={ images[0] }
+        ></Image>
+      </div>
+    );
+  })
+  .add('with alt text', () => {
+    const images = files('Image', 'image/*', []);
+
+    return (
+      <div style={{ width: '300px' }}>
+        <Image
+          src={ images[0] }
+          alt="Alternative text when loading fails"
+        ></Image>
+      </div>
+    );
+  })
+  .add('with simple caption', () => {
+    const images = files('Image', 'image/*', [ dummyImage ]);
+    const captionText = text('Caption text', 'A simple caption text');
+
+    return (
+      <div style={{ width: '300px' }}>
+        <Image
+          src={ images[0] }
+          caption={ captionText }
+        ></Image>
+      </div>
+    );
+  })
+  .add('with complex caption', () => {
+    const images = files('Image', 'image/*', [ dummyImage ]);
+
+    return (
+      <div style={{ width: '300px' }}>
+        <Image
+          src={ images[0] }
+          caption={
+          <span>
+            <a href="//lucascranach.org/">A link as caption</a>
+            <br />
+            <a href="//th-koeln">A second link as caption</a>
+            <br />
+            <input style={ {
+              position: 'absolute',
+              right: '-1rem',
+              bottom: '-0.25rem',
+            } } type="checkbox" />
+          </span>
+        }
+        ></Image>
+      </div>
+    );
+  });
