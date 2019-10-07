46c52985feada15e26d5e8e4f1ed608a9744ea06
Jorge H. F. Pereira <j.pereira@klickmeister.de>
2019-10-07 17:25:50 +0200

    Storybook: Remove parameters for intro stories

--- src/__stories__/intro.stories.jsx
+++ src/__stories__/intro.stories.jsx
@@ -2,12 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 storiesOf('Global|Overview', module)
-  .addParameters({
-    options: {
-      showPanel: false,
-      isToolshown: false,
-    },
-  })
   .add('Welcome', () => (
     <div>Welcome</div>
   ));
