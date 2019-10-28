6f777a35f3969a8e3f558cf66f2aff04571cc8bf
Jorge H. F. Pereira <j.pereira@klickmeister.de>
2019-10-28 23:03:53 +0100

    Image: Erweitere um Tests für Caption

--- src/components/atoms/image/image.test.jsx
+++ src/components/atoms/image/image.test.jsx
@@ -7,6 +7,7 @@ import Image from '.';
 describe('Atoms/Image', () => {
   /* Element selectors */
   const imageSelector = '[data-component="atoms/image"]';
+  const imageCaptionSelector = '.image-caption';
 
   it('renders correctly', () => {
     const { container } = render(
@@ -14,5 +15,25 @@ describe('Atoms/Image', () => {
     );
 
     expect(!!container.querySelector(imageSelector)).toBe(true);
+  });
+
+  it('shows image caption if given', () => {
+    const captionText = 'Example caption text';
+
+    const firstRender = render(
+      <Image/>,
+    );
+
+    expect(!!firstRender.container.querySelector(imageCaptionSelector)).toBe(false);
+    expect(!!firstRender.queryByText(captionText)).toBe(false);
+
+    const secondRender = render(
+      <Image
+        caption={ captionText }
+      />,
+    );
+
+    expect(!!secondRender.container.querySelector(imageCaptionSelector)).toBe(true);
+    expect(secondRender.getByText(captionText)).toHaveTextContent(captionText);
   });
 });
