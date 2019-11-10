42a19418d6311b18ad9c23de2d092e276a774d6f
Jorge H. F. Pereira <j.pereira@klickmeister.de>
2019-11-10 20:31:58 +0100

    Navigation: Add first test

---
+++ src/components/molecules/navigation/navigation.test.jsx
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Navigation from '.';
+
+
+describe('Molecules/GraphicCard', () => {
+  /* Element selectors */
+  const navigationSelector = '.navigation';
+  const navigationBrandSelector = '.navbar-brand';
+  const navigationMenuSelector = '.navbar-menu';
+  const navigationEndSelector = '.navbar-end';
+
+  it('renders correctly', () => {
+    const { container } = render(
+      <Navigation/>,
+    );
+
+    expect(!!container.querySelector(navigationSelector)).toBe(true);
+    expect(!!container.querySelector(navigationBrandSelector)).toBe(true);
+    expect(!!container.querySelector(navigationMenuSelector)).toBe(true);
+    expect(!!container.querySelector(navigationEndSelector)).toBe(true);
+  });
+});
