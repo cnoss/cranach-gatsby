ab60f7d2059b559e12bf4f86be2b72417dc2c9e8
Jorge H. F. Pereira <j.pereira@klickmeister.de>
2019-11-29 22:34:30 +0100

    Leporello-Graphic-Item: Extend basic storybook story and tests

--- src/components/molecules/leporello-graphic-item/leporello-graphic-item.test.jsx
+++ src/components/molecules/leporello-graphic-item/leporello-graphic-item.test.jsx
@@ -1,12 +1,14 @@
 
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import LeporelloGraphicItem from '.';
 
 describe('Molecules/LeporelloGraphicItem', () => {
   /* Element selectors */
   const leporelloGraphicItemSelector = '[data-component="molecules/leporello-graphic-item"]';
+  const togglerSelector = '[data-component="atoms/toggler"]';
+  const isOpenSelector = '.-is-open';
 
   it('renders correctly', () => {
     const { container } = render(
@@ -14,5 +16,31 @@ describe('Molecules/LeporelloGraphicItem', () => {
     );
 
     expect(!!container.querySelector(leporelloGraphicItemSelector)).toBe(true);
+  });
+
+  it('is toggled on button click', () => {
+    const spy = jest.fn();
+
+    const { container } = render(
+      <LeporelloGraphicItem onToggle={ spy } />,
+    );
+
+    const togglerEl = container.querySelector(togglerSelector);
+
+    expect(spy).toHaveBeenCalledTimes(0);
+
+    expect(!!container.querySelector(isOpenSelector)).toBe(false);
+
+    fireEvent.click(togglerEl);
+
+    expect(!!container.querySelector(isOpenSelector)).toBe(true);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(true);
+
+    fireEvent.click(togglerEl);
+
+    expect(spy).toHaveBeenCalledTimes(2);
+    expect(spy).toHaveBeenCalledWith(false);
   });
 });
