f3f87a96b686001737a82f24c6ef00ac56cf1ac4
Jorge H. F. Pereira <j.pereira@klickmeister.de>
2019-11-29 21:10:25 +0100

    Leporello: Extend basic storybook story and tests

--- src/components/atoms/leporello/leporello.stories.jsx
+++ src/components/atoms/leporello/leporello.stories.jsx
@@ -8,5 +8,17 @@ import Leporello from '.';
 
 storiesOf('Components|Atoms/Leporello', module)
   .add('default', () => (
-    <Leporello />
+    <Leporello>
+      <Leporello.Item>
+        First Leporello item
+      </Leporello.Item>
+
+      <Leporello.Item>
+        Second Leporello item
+      </Leporello.Item>
+
+      <Leporello.Item>
+        Third Leporello item
+      </Leporello.Item>
+    </Leporello>
   ));
