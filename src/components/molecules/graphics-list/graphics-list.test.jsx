fe189aa7b5abb32847f9f5448acd66f3c21bf53f
Jorge H. F. Pereira <j.pereira@klickmeister.de>
2019-12-01 23:11:34 +0100

    Graphics-List: Extend unit tests

--- src/components/molecules/graphics-list/graphics-list.test.jsx
+++ src/components/molecules/graphics-list/graphics-list.test.jsx
@@ -4,9 +4,12 @@ import { render } from '@testing-library/react';
 
 import GraphicsList from '.';
 
+const dummyGraphicItems = require('./example-data');
+
 describe('Molecules/GraphicsList', () => {
   /* Element selectors */
   const graphicsListSelector = '[data-component="molecules/graphics-list"]';
+  const graphicItemSelector = '.graphic-item';
 
   it('renders correctly', () => {
     const { container } = render(
@@ -14,5 +17,23 @@ describe('Molecules/GraphicsList', () => {
     );
 
     expect(!!container.querySelector(graphicsListSelector)).toBe(true);
+  });
+
+  it('renders the correct number of graphic items', () => {
+    const firstRender = render(
+      <GraphicsList items={ [] } />,
+    );
+
+    const firstRenderItemEls = firstRender.container.querySelectorAll(graphicItemSelector);
+
+    expect(firstRenderItemEls.length).toBe(0);
+
+    const secondRender = render(
+      <GraphicsList items={ dummyGraphicItems } />,
+    );
+
+    const secondRenderItemEls = secondRender.container.querySelectorAll(graphicItemSelector);
+
+    expect(secondRenderItemEls.length).toBe(dummyGraphicItems.length);
   });
 });
