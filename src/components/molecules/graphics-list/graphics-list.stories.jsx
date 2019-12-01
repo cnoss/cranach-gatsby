b516057220e0fbb83af6316c0867739278263e1f
Jorge H. F. Pereira <j.pereira@klickmeister.de>
2019-12-01 23:05:46 +0100

    Graphics-List: Add default storybook stories

--- src/components/molecules/graphics-list/graphics-list.stories.jsx
+++ src/components/molecules/graphics-list/graphics-list.stories.jsx
@@ -3,10 +3,40 @@ import { storiesOf } from '@storybook/react';
 
 import '~/styles/main.scss';
 
+import {
+  withKnobs,
+  number,
+} from '@storybook/addon-knobs';
+
 import GraphicsList from '.';
 
+const dummyItems = require('./example-data');
 
 storiesOf('Components|Molecules/GraphicsList', module)
-  .add('default', () => (
-    <GraphicsList />
-  ));
+  .addDecorator(withKnobs)
+  .add('without items', () => (
+    <GraphicsList items={ [] } />
+  ))
+  .add('with dynamic number of items', () => {
+
+    const graphicsNumber = number('Number of graphics', 5, {
+      range: true,
+      min: 1,
+      max: 50,
+      step: 1,
+    });
+
+    const dynItems = (new Array(graphicsNumber).fill(0)).map((_, i) => {
+      const randomIdx = Math.floor(Math.random() * dummyItems.length);
+      const dummyItem = dummyItems[randomIdx];
+
+      return {
+        ...dummyItem,
+        imgSrc: `${dummyItem.imgSrc}#${i}`,
+      };
+    });
+
+    return (
+      <GraphicsList items={ dynItems } />
+    );
+  });
