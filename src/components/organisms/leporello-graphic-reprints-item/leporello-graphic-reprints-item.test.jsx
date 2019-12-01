760cdad53ee193bd9b672bc359391f1769b34f9c
Jorge H. F. Pereira <j.pereira@klickmeister.de>
2019-12-01 22:29:43 +0100

    Leporello-Graphic-Reprints-Item: Extend unit tests

--- src/components/organisms/leporello-graphic-reprints-item/leporello-graphic-reprints-item.test.jsx
+++ src/components/organisms/leporello-graphic-reprints-item/leporello-graphic-reprints-item.test.jsx
@@ -1,33 +1,42 @@
 
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import LeporelloGraphicReprintsItem from '.';
 
-const slimDummyGraphic = {
-  references: [
-    {
-      ref: {
-        owner: 'Me',
-        langCode: 'de',
-        slug: 'slug',
-        image: {
-          small: 'http://url-to.image/image.jpeg',
-        },
-      },
-    },
-  ],
-};
+const dummyGraphic = require('./example-data');
 
 describe('Organisms/LeporelloGraphicReprintsItem', () => {
   /* Element selectors */
   const leporelloGraphicReprintsItemSelector = '[data-component="organisms/leporello-graphic-reprints-item"]';
+  const graphicsListItemSelector = '[data-component="molecules/graphics-list"] .graphic-item';
+  const togglerSelector = '[data-component="atoms/toggler"]';
+
+  const reprintsItemsLimit = 4;
 
   it('renders correctly', () => {
     const { container } = render(
-      <LeporelloGraphicReprintsItem graphic={ slimDummyGraphic } />,
+      <LeporelloGraphicReprintsItem graphic={ dummyGraphic } />,
     );
 
     expect(!!container.querySelector(leporelloGraphicReprintsItemSelector)).toBe(true);
+  });
+
+  it('renders the correct number of reprint items when closed and opened', () => {
+    const { container } = render(
+      <LeporelloGraphicReprintsItem graphic={ dummyGraphic } />,
+    );
+
+    const reprintItemEls = container.querySelectorAll(graphicsListItemSelector);
+
+    expect(reprintItemEls.length).toBe(reprintsItemsLimit);
+
+    const togglerEl = container.querySelector(togglerSelector);
+
+    fireEvent.click(togglerEl);
+
+    const newReprintItemEls = container.querySelectorAll(graphicsListItemSelector);
+
+    expect(newReprintItemEls.length).toBe(dummyGraphic.references.length);
   });
 });
