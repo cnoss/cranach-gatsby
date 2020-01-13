0f3c801c82414e5058e02a0d9d296598db25d88d
Jorge H. F. Pereira <j.pereira@klickmeister.de>
2020-01-13 03:22:38 +0100

    VirtualObjectPage: Group and output reprints by condition level

--- src/templates/virtual-object-page.jsx
+++ src/templates/virtual-object-page.jsx
@@ -17,12 +17,71 @@ import i18n from '~/i18n';
 const PageTemplate = ({ pageContext }) => {
   const graphic = pageContext;
   const title = (graphic.titles[0] && graphic.titles[0].title) || '';
-  const [selectedReprintItem, setSelectedReprintItem] = useState(null);
-  const [selectedRelatedWorkItem, setSelectedRelatedWorkItem] = useState(null);
 
   i18n(graphic.langCode);
 
   const { t } = useTranslation('VirtualObjectPage');
+
+  const expectedReprintConditionLevelGroups = [
+    {
+      id: '1st-level',
+      translations: {
+        title: 'Reprints',
+        subtitle: '1st state',
+        description: '1st state description',
+      },
+      filter: reprintItem => reprintItem.ref && [0, 1].includes(reprintItem.ref.conditionLevel),
+      items: [],
+    },
+    {
+      id: '2nd-level',
+      translations: {
+        title: 'Reprints',
+        subtitle: '2nd state',
+        description: '2nd state description',
+      },
+      filter: reprintItem => reprintItem.ref && reprintItem.ref.conditionLevel === 2,
+      items: [],
+    },
+    {
+      id: '3rd-level',
+      translations: {
+        title: 'Reprints',
+        subtitle: '3rd state',
+        description: '3rd state description',
+      },
+      filter: reprintItem => reprintItem.ref && reprintItem.ref.conditionLevel === 3,
+      items: [],
+    },
+  ];
+
+  const reprintConditionLevelGroups = expectedReprintConditionLevelGroups.map(
+    conditionLevelGroup => ({
+      ...conditionLevelGroup,
+      items: graphic.references.reprints.filter(
+        conditionLevelGroup.filter,
+      ),
+      selectedReprintItem: null,
+    }),
+  ).filter(group => group.items.length > 0);
+
+  const [reprintGroups, setReprintGroups] = useState(reprintConditionLevelGroups);
+  const [selectedRelatedWorkItem, setSelectedRelatedWorkItem] = useState(null);
+
+  const updateSelectedReprintItemForReprintGroup = (selectedReprintItem, groupIdx) => {
+    const updatedReprintGroups = reprintGroups.map((reprintGroup, idx) => {
+      if (idx === groupIdx) {
+        return {
+          ...reprintGroup,
+          selectedReprintItem,
+        };
+      }
+
+      return reprintGroup;
+    });
+
+    setReprintGroups(updatedReprintGroups);
+  };
 
   return (
     <div
@@ -41,22 +100,30 @@ const PageTemplate = ({ pageContext }) => {
         <Leporello>
           <LeporelloGraphicDetailsItem graphic={graphic} />
 
-          {selectedReprintItem
-            ? (
-              <LeporelloGraphicRealItem
-                graphic={selectedReprintItem}
-                onClose={() => setSelectedReprintItem(null)}
-              />
-            )
-            : (
-              <LeporelloGraphicReprintsItem
-                title={t('Reprints')}
-                subtitle={t('1st state')}
-                description={t('1st state description')}
-                reprints={graphic.references.reprints}
-                onItemClick={setSelectedReprintItem}
-              />
-            )
+          {
+            reprintGroups.map((reprintGroup, idx) => {
+              if (reprintGroup.selectedReprintItem) {
+                return (
+                  <LeporelloGraphicRealItem
+                    key={`${reprintGroup.id}-real-item`}
+                    graphic={reprintGroup.selectedReprintItem}
+                    onClose={() => updateSelectedReprintItemForReprintGroup(null, idx)}
+                  />
+                );
+              }
+
+              return (<LeporelloGraphicReprintsItem
+                key={`${reprintGroup.id}-reprint-item`}
+                title={t(reprintGroup.translations.title)}
+                subtitle={t(reprintGroup.translations.subtitle)}
+                description={t(reprintGroup.translations.description)}
+                reprints={reprintGroup.items}
+                onItemClick={reprintItem => updateSelectedReprintItemForReprintGroup(
+                  reprintItem,
+                  idx,
+                )}
+              />);
+            })
           }
 
           {graphic.references.relatedWorks.length > 0
