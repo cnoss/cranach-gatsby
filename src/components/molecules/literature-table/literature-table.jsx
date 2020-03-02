2c99d93031615bbdc10e943ae40142e4561aa0bc
Jorge H. F. Pereira <jhfpereira@users.noreply.github.com>
2020-03-02 01:18:31 +0100

    LiteratureTable: Introduce component

---
+++ src/components/molecules/literature-table/literature-table.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+import DynamicTable from '~/components/atoms/dynamic-table';
+import './literature-table.scss';
+
+export default ({ data }) => {
+  const { t } = useTranslation('LiteratureTable');
+
+  return (
+  <div
+    className="literature-table"
+    data-component="molecules/literature-table"
+  >
+    <DynamicTable
+      data={ data }
+      columns={ [
+        { field: 'title', title: '' },
+        { field: 'pageNumber', title: t('reference on page') },
+      ] }
+    />
+  </div>);
+};
