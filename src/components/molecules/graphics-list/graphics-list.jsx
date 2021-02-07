cfb249278f4e176ed0bcbf7a5e6fe0ac6306d0d4
cnoss <christian.noss@fh-koeln.de>
2021-02-07 11:38:21 +0100

    Index bei den Keys addiert
    Weil sich gatsby anderfalls beschwert hat, das die Keys nicht unique sind. Müsste man bei Gelegenheit noch mal prüfen.

--- src/components/molecules/graphics-list/graphics-list.jsx
+++ src/components/molecules/graphics-list/graphics-list.jsx
@@ -5,46 +5,44 @@ import Image from '~/components/atoms/image';
 
 import './graphics-list.scss';
 
+
 export default ({
   items,
   onItemClick,
-}) => (
-    <ul
-      className="graphics-list"
-      data-component="molecules/graphics-list"
-    >
-      {
-        items.map(
-          item => <li
-            key={item.imgSrc}
-            className="graphics-list-item"
-          >
-            <Link
-              to={item.to}
-              triggersInternalTransition={!!item.triggersInternalTransition}
-              onClick={(e) => {
-                if (item.preventLinkFollowing) {
-                  if ((typeof onItemClick) === 'function') {
-                    onItemClick(item);
-                  }
-                  e.preventDefault();
-                  return false;
+}) => (<ul
+    className="graphics-list"
+    data-component="molecules/graphics-list"
+  >
+    {
+      items.map(
+        (item, index) => <li
+          key={`${item.inventoryNumber}-${index}`}
+          className="graphics-list-item"
+        >
+          <Link
+            to={item.to}
+            triggersInternalTransition={!!item.triggersInternalTransition}
+            onClick={(e) => {
+              if (item.preventLinkFollowing) {
+                if ((typeof onItemClick) === 'function') {
+                  onItemClick(item);
                 }
+                e.preventDefault();
+                return false;
+              }
 
-                return true;
-              }}
-            >
-              <Image
-                src={item.imgSrc}
-                alt={item.title}
-                classNamePrefix={'graphics-list-item__image'}
-                caption={item.title}
-                modifierWithBox={true}
-              />
-            </Link>
-          </li>,
-        )
-      }
-    </ul>
-
-);
+              return true;
+            }}
+          >
+            <Image
+              src={item.imgSrc}
+              alt={item.title}
+              classNamePrefix={'graphics-list-item__image'}
+              caption={item.title}
+              modifierWithBox={true}
+            />
+          </Link>
+        </li>,
+      )
+    }
+  </ul >);
