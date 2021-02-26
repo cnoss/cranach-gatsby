10912ad9d34d540b8302bf2d9f6347d7fe127ad3
Jorge H. F. Pereira <jhfpereira@users.noreply.github.com>
2021-02-26 11:17:35 +0100

    fix(search): move mapping code to artifact server for testing purposes

--- src/api/globalSearch.js
+++ src/api/globalSearch.js
@@ -20,68 +20,9 @@ export const searchGloballyFor = term => new Promise((resolve) => {
 */
 
 
-const mapGraphicToArtifact = (item) => {
-  const inventor = item.involvedPersons.find(person => person.role === 'Inventor');
-
-  return {
-    inventoryNumber: item.inventoryNumber,
-    title: (item.titles[0] && item.titles[0].title) || '',
-    subtitle: inventor ? inventor.name : ' ',
-    date: item.dating.dated || '',
-    additionalInfoList: [
-      `${item.classification.classification}, ${item.classification.printProcess}`,
-      item.dimensions,
-    ],
-    classification: item.classification.classification,
-    to: `/${item.langCode}/${item.inventoryNumber}`,
-    imgSrc: (item && item.images && item.images.sizes.s && item.images.sizes.s.src),
-  };
-};
-
-const mapPaintingToArtifact = (item) => {
-  const inventor = item.involvedPersons.find(person => person.role === 'Künstler');
-
-  return {
-    inventoryNumber: item.inventoryNumber,
-    title: (item.titles[0] && item.titles[0].title) || '',
-    subtitle: inventor ? inventor.name : ' ',
-    date: item.dating.dated || '',
-    additionalInfoList: [
-      item.classification.classification,
-      item.dimensions,
-    ],
-    classification: item.classification.classification,
-    to: `/${item.langCode}/${item.inventoryNumber}`,
-    imgSrc: (item && item.images && item.images.sizes.s && item.images.sizes.s.src),
-  };
-};
-
-const mapArchivalToArtifact = (item) => {
-  const summary = item.summaries[0] || '';
-  const splitWords = summary.split(' ');
-  const title = splitWords.length < 25 ? summary : `${splitWords.slice(0, 24).join(' ')} ...`;
-
-  return {
-    inventoryNumber: item.inventoryNumber,
-    title,
-    subtitle: ' ',
-    date: (item.dating && item.dating.dated) || '',
-    additionalInfoList: [],
-    classification: '',
-    to: `/${item.langCode}/${item.inventoryNumber}`,
-    imgSrc: '',
-  };
-};
-
-
 const searchGloballyRandomizedByLang = lang => fetch(
   `http://localhost:3000/random?lang=${lang}`,
-).then(resp => resp.json())
-  .then(result => ({
-    graphics: result.graphics.map(mapGraphicToArtifact),
-    paintings: result.paintings.map(mapPaintingToArtifact),
-    archivals: result.archivals.map(mapArchivalToArtifact),
-  }));
+).then(resp => resp.json());
 
 
 export default {
