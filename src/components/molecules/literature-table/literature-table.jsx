22c3a25406ede1e647812f0e7105b3d7c0074bf5
Jorge H. F. Pereira <jhfpereira@users.noreply.github.com>
2021-05-02 15:07:06 +0200

    fix(literature-table): use unique key for alternateNumbers loop and remove tbody-whitespace

--- src/components/molecules/literature-table/literature-table.jsx
+++ src/components/molecules/literature-table/literature-table.jsx
@@ -60,103 +60,70 @@ export default ({
               <td className="literature-table-datum" colSpan={ forPrimary ? 2 : 4 }>
 
                 <table className="literature-item-details-table">
-                  <tbody>
-                    {
-                      Object.entries(item.roles).map(([role, names]) => (<tr key={ role }>
+                  <tbody>{ Object.entries(item.roles).map(([role, names]) => (<tr key={ role }>
                         <td className="literature-item-details-table-title">{ t(mapRoleToTranslationCode(role)) }</td>
                         <td>{ names.join(', ') }</td>
                       </tr>))
-                    }
-
-                    { item.title && (<tr>
+                    }{ (item.title && (<tr>
                         <td className="literature-item-details-table-title">{ t('Title') }</td>
                         <td dangerouslySetInnerHTML={{ __html: item.title }}></td>
-                      </tr>)
-                    }
-
-                    { item.pageNumbers && (<tr>
+                      </tr>)) || null
+                    }{ (item.pageNumbers && (<tr>
                         <td className="literature-item-details-table-title">{ t('Pages') }</td>
                         <td>{ item.pageNumbers }</td>
-                      </tr>)
-                    }
-
-                    { item.series && (<tr>
+                      </tr>)) || null
+                    }{ (item.series && (<tr>
                         <td className="literature-item-details-table-title">{ t('Series') }</td>
                         <td>{ item.series }</td>
-                      </tr>)
-                    }
-
-                    { item.volume && (<tr>
+                      </tr>)) || null
+                    }{ (item.volume && (<tr>
                         <td className="literature-item-details-table-title">{ t('Volume') }</td>
                         <td>{ item.volume }</td>
-                      </tr>)
-                    }
-
-                    { item.journal && (<tr>
+                      </tr>)) || null
+                    }{ (item.journal && (<tr>
                         <td className="literature-item-details-table-title">{ t('Journal') }</td>
                         <td>{ item.journal }</td>
-                      </tr>)
-                    }
-
-                    { item.issue && (<tr>
+                      </tr>)) || null
+                    }{ (item.issue && (<tr>
                         <td className="literature-item-details-table-title">{ t('Issue') }</td>
                         <td>{ item.issue }</td>
-                      </tr>)
-                    }
-
-                    { item.publication && (<tr>
+                      </tr>)) || null
+                    }{ (item.publication && (<tr>
                         <td className="literature-item-details-table-title">{ t('Publication') }</td>
                         <td dangerouslySetInnerHTML={{ __html: item.publication }}></td>
-                      </tr>)
-                    }
-
-                    { item.publishLocation && (<tr>
+                      </tr>)) || null
+                    }{ (item.publishLocation && (<tr>
                         <td className="literature-item-details-table-title">{ t('Place of publication') }</td>
                         <td>{ item.publishLocation }</td>
-                      </tr>)
-                    }
-
-                    { item.publishDate && (<tr>
+                      </tr>)) || null
+                    }{ (item.publishDate && (<tr>
                         <td className="literature-item-details-table-title">{ t('Year of publication') }</td>
                         <td>{ item.publishDate }</td>
-                      </tr>)
-                    }
-
-                    {
-                      item.periodOfOrigin && (<tr>
+                      </tr>)) || null
+                    }{
+                      (item.periodOfOrigin && (<tr>
                         <td className="literature-item-details-table-title">{ t('Period of Origin') }</td>
                         <td>{ item.periodOfOrigin }</td>
-                      </tr>)
-                    }
-
-                    {
-                      item.physicalDescription && (<tr>
+                      </tr>)) || null
+                    }{ (item.physicalDescription && (<tr>
                         <td className="literature-item-details-table-title">{ t('Book format') }</td>
                         <td>{ item.physicalDescription }</td>
-                      </tr>)
-                    }
-
-                    { item.mention && (<tr>
+                      </tr>)) || null
+                    }{ (item.mention && (<tr>
                         <td className="literature-item-details-table-title">{ t('Mention') }</td>
                         <td dangerouslySetInnerHTML={{ __html: item.mention }}></td>
-                      </tr>)
-                    }
-
-                    { item.link && (<tr>
+                      </tr>)) || null
+                    }{ (item.link && (<tr>
                         <td className="literature-item-details-table-title">{ t('Link') }</td>
                         <td>{ item.link }</td>
-                      </tr>)
-                    }
-
-                    {
-                      item.alternateNumbers.length > 0 && item.alternateNumbers.map(
-                        (alternateNumber) => (<tr>
+                      </tr>)) || null
+                    }{ (item.alternateNumbers.length > 0 && item.alternateNumbers.map(
+                      (alternateNumber) => (<tr key={alternateNumber.description}>
                           <td className="literature-item-details-table-title">{ t(alternateNumber.description) }</td>
                           <td>{ alternateNumber.number }</td>
                         </tr>),
-                      )
-                    }
-                  </tbody>
+                    )) || null
+                  }</tbody>
                 </table>
 
               </td>
